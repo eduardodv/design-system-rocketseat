e7322ee743667858b5cee4bb97d9f7b67ad3bc33
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2023-12-08 00:29:11 -0300

    Projeto 5 - módulo 2

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,38 @@
+// import { ComponentProps } from 'react'
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  '&:hover': {
+    backgroundColor: '$ignite500',
+  },
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = {
+  size: 'small' | 'big'
+}
+
+// export type ButtonProps = ComponentProps<typeof Button>
