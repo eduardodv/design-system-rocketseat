335db112d40fb7b139bae4faa5a9410de1412624
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2024-01-11 11:14:10 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,5 +25,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system-rocketseat'
+    }
+
+    return config
+  }
 };
 export default config;
